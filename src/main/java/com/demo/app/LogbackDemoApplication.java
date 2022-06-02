88aba8118f674e38675a8651df6fec265fe0d2e8
Suchintan Baral <58394238+suchintanb@users.noreply.github.com>
2022-06-02 01:02:28 -0500

    Update LogbackDemoApplication.java

--- src/main/java/com/demo/app/LogbackDemoApplication.java
+++ src/main/java/com/demo/app/LogbackDemoApplication.java
@@ -11,3 +11,177 @@ public class LogbackDemoApplication {
 	}
 
 }
+{
+    "vendorNode": {
+        "vendorNumber": "112062",
+        "vendorType": "2",
+        "companyDBAName": "EAST COAST MATERIAL MANAGEMENT",
+        "vendorName": "EAST COAST MATERIAL MANAGEMENT",
+        "gln": "0861010000202",
+        "contactTypes": {},
+        "dunsNumber": "61692346",
+        "vendorStatus": "ACT",
+        "companyWebsite": " ",
+        "obsolete": false,
+        "migrated": false,
+        "contractualMaintenaceInProgress": false,
+        "displayProgramType": "",
+        "dataSubmissionInProgress": false,
+        "postSubmissionAndPreOnboarding": false,
+        "activeContract": false,
+        "canadianVendor": false,
+        "migrationInProgress": false,
+        "maintenanceInProgress": false,
+        "onboradingCompleteOrActiveVendor": false,
+        "contractInitiated": false,
+        "usvendor": false,
+        "vendorLocked": false,
+        "active": false
+    },
+    "returnToVendorDetails": {
+        "returnToVendor": {
+            "returnMethod": "dispose",
+            "returnAuthorization": "noReturnAuthorization",
+            "collectionMethod": "vendorDebit",
+            "defectiveAllowancePercentage": 0.0000,
+            "blanketRACode": "",
+            "hazmat": "",
+            "liveGoods": "",
+            "outForRepair": "",
+            "specialInstruction": "DEFECTIVE FOLLOW POLICY, NON-DEFECTIVE SALE ON THE SALES FLOOR"
+        }
+    },
+    "address": {
+        "id": "1187355",
+        "addressLineOne": "46 LATHROP ROAD EXT",
+        "addressLineTwo": "",
+        "addressLineThree": "",
+        "city": "PLAINFIELD",
+        "zipCode": "06374",
+        "state": "CT",
+        "country": "US",
+        "timeZone": "1",
+        "mailType": "MAIL_AND_PARCEL"
+    },
+    "contacts": {
+        "2": {}
+    },
+    "childVendorAttributes": [
+        {
+            "vendorNode": {
+                "vendorNumber": "112062",
+                "vendorType": "4",
+                "companyDBAName": "EAST COAST MATERIAL MANAGEMENT",
+                "vendorName": "EAST COAST MATERIAL MANAGEMENT",
+                "contactTypes": {},
+                "dunsNumber": "61692346",
+                "vendorStatus": "ACT",
+                "obsolete": false,
+                "migrated": false,
+                "contractualMaintenaceInProgress": false,
+                "displayProgramType": "",
+                "dataSubmissionInProgress": false,
+                "postSubmissionAndPreOnboarding": false,
+                "activeContract": false,
+                "canadianVendor": false,
+                "migrationInProgress": false,
+                "maintenanceInProgress": false,
+                "onboradingCompleteOrActiveVendor": false,
+                "contractInitiated": false,
+                "usvendor": false,
+                "vendorLocked": false,
+                "active": false
+            },
+            "address": {
+                "id": "1187356",
+                "addressLineOne": "46 LATHROP ROAD EXT",
+                "addressLineTwo": "",
+                "addressLineThree": "",
+                "city": "PLAINFIELD",
+                "zipCode": "06374",
+                "state": "CT",
+                "country": "US",
+                "timeZone": "1",
+                "mailType": "MAIL_AND_PARCEL"
+            },
+            "childVendorAttributes": [
+                {
+                    "vendorNode": {
+                        "vendorNumber": "112062",
+                        "vendorType": "3",
+                        "companyDBAName": "EAST COAST MATERIAL MANAGEMENT",
+                        "vendorName": "EAST COAST MATERIAL MANAGEMENT",
+                        "contactTypes": {},
+                        "dunsNumber": "",
+                        "vendorStatus": "ACT",
+                        "obsolete": false,
+                        "migrated": false,
+                        "contractualMaintenaceInProgress": false,
+                        "displayProgramType": "",
+                        "dataSubmissionInProgress": false,
+                        "postSubmissionAndPreOnboarding": false,
+                        "activeContract": false,
+                        "canadianVendor": false,
+                        "migrationInProgress": false,
+                        "maintenanceInProgress": false,
+                        "onboradingCompleteOrActiveVendor": false,
+                        "contractInitiated": false,
+                        "usvendor": false,
+                        "vendorLocked": false,
+                        "active": false
+                    },
+                    "address": {
+                        "id": "1187357",
+                        "addressLineOne": "46 LATHROP ROAD EXT",
+                        "addressLineTwo": "",
+                        "addressLineThree": "",
+                        "city": "PLAINFIELD",
+                        "zipCode": "06374",
+                        "state": "CT",
+                        "country": "US",
+                        "timeZone": "1",
+                        "mailType": "MAIL_AND_PARCEL"
+                    }
+                },
+                {
+                    "vendorNode": {
+                        "vendorNumber": "112062",
+                        "vendorType": "1",
+                        "companyDBAName": "EAST COAST MATERIAL MANAGEMENT",
+                        "vendorName": "EAST COAST MATERIAL MANAGEMENT",
+                        "contactTypes": {},
+                        "dunsNumber": "",
+                        "vendorStatus": "ACT",
+                        "obsolete": false,
+                        "migrated": false,
+                        "contractualMaintenaceInProgress": false,
+                        "displayProgramType": "",
+                        "dataSubmissionInProgress": false,
+                        "postSubmissionAndPreOnboarding": false,
+                        "activeContract": false,
+                        "canadianVendor": false,
+                        "migrationInProgress": false,
+                        "maintenanceInProgress": false,
+                        "onboradingCompleteOrActiveVendor": false,
+                        "contractInitiated": false,
+                        "usvendor": false,
+                        "vendorLocked": false,
+                        "active": false
+                    },
+                    "address": {
+                        "id": "1187357",
+                        "addressLineOne": "46 LATHROP ROAD EXT",
+                        "addressLineTwo": "",
+                        "addressLineThree": "",
+                        "city": "PLAINFIELD",
+                        "zipCode": "06374",
+                        "state": "CT",
+                        "country": "US",
+                        "timeZone": "1",
+                        "mailType": "MAIL_AND_PARCEL"
+                    }
+                }
+            ]
+        }
+    ]
+}
